1da0e1737b3bf76442d3bc82ccae2b9d0883e838
Edson Martins <aekmartins@gmail.com>
2024-05-24 09:47:47 -0300

    create initial structure (#1)

---
+++ src/main/java/com/weltonramos/notification_service/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.weltonramos.notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+}
